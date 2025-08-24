6b45e5b39aa334709c5a4aa852314ab17d5addf4
Aditya Tikone <dtikone5@gmail.com>
2025-08-25 00:43:03 +0530

    BACK

--- my-app/src/services/api.js
+++ my-app/src/services/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "https://tikonecricketgurukul.vercel.app";
+const API_BASE_URL = "https://tikonecricketgurukulbackend.onrender.com";
 
 // Helper function to handle API responses
 const handleResponse = async (response) => {
