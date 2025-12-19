892a1b9ca7ae933544110185f4843d351f80705e
AdityaTikone8686 <gigaminex@gmail.com>
2025-12-19 14:05:54 +0530

    Update ShopPage.jsx

--- my-app/src/pages/ShopPage.jsx
+++ my-app/src/pages/ShopPage.jsx
@@ -81,7 +81,7 @@ const ShopPage = () => {
 
 
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
-          {products.map((product, index) => (
+          {FilteredProducts.map((product, index) => (
             <div
               key={index}
               className="bg-white rounded-2xl shadow-md p-3 hover:shadow-lg transition flex flex-col"
