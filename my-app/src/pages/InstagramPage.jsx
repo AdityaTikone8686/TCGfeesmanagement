f66c59773621b917b82b1b99bdeb11c71ec74ce5
AdityaTikone8686 <gigaminex@gmail.com>
2025-12-25 17:22:47 +0530

    Create InstagramPage.jsx

---
+++ my-app/src/pages/InstagramPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import InstagramFeed from "../components/InstagramFeed";
+import Layout from "../components/layout/Layout";
+
+const InstagramPage = () => {
+  return (
+    <Layout>
+      <InstagramFeed />
+    </Layout>
+  );
+};
+
+export default InstagramPage;
