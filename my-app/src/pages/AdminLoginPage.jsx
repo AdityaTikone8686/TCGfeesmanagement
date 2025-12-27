e006057b35e1a2350576212c22497b21f95e0da3
AdityaTikone8686 <gigaminex@gmail.com>
2025-12-27 19:48:34 +0530

    Update AdminLoginPage.jsx

--- my-app/src/pages/AdminLoginPage.jsx
+++ my-app/src/pages/AdminLoginPage.jsx
@@ -10,7 +10,7 @@ export default function AdminLoginPage() {
 
   const handleLogin = async () => {
     try {
-      const res = await fetch("/api/admin/auth/login", {
+      const res = await fetch("/api/admin/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
