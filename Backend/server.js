9e5156bcd62ecd93b832d8428534468a366749ce
AdityaTikone8686 <gigaminex@gmail.com>
2025-12-27 15:27:56 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -12,8 +12,8 @@ import adminRoutes from "./routes/adminRoutes.js";
 import userPaymentStatusRoutes from "./routes/userPaymentStatusRoutes.js";
 import subscriptionRoutes from "./routes/subscriptionRoutes.js";
 import paymentRequestRoutes from "./routes/paymentRequestRoutes.js";
-import instagramRoutes from "./routes/instagram.js";
-import socialRoutes from "./routes/socialRoutes.js";
+
+
 
 dotenv.config();
 
@@ -36,8 +36,8 @@ app.use("/api/admin", adminRoutes);
 app.use("/api/payment-status", userPaymentStatusRoutes);
 app.use("/api/subscriptions", subscriptionRoutes);
 app.use("/api/payment-requests", paymentRequestRoutes);
-app.use("/api/instagram", instagramRoutes);
-app.use("/api/social", socialRoutes);
+
+
 
 // Root endpoint
 app.get("/", (req, res) => {
