1bdb75e9bf5e8c77057b76533ccb0af48adb8fd2
AdityaTikone8686 <gigaminex@gmail.com>
2025-12-27 15:19:33 +0530

    Create SocialPage.jsx

---
+++ my-app/src/pages/SocialPage.jsx
@@ -0,0 +1,10 @@
+import SocialFeed from "../components/SocialFeed";
+
+export default function SocialPage() {
+  return (
+    <div className="container mx-auto py-10">
+      <h1 className="text-3xl font-bold mb-6">Moments at TCG 📸</h1>
+      <SocialFeed />
+    </div>
+  );
+}
