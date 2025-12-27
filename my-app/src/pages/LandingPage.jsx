db2dd53a2274c839edd96ff598c040cf4509fd1e
AdityaTikone8686 <gigaminex@gmail.com>
2025-12-27 15:47:03 +0530

    Update LandingPage.jsx

--- my-app/src/pages/LandingPage.jsx
+++ my-app/src/pages/LandingPage.jsx
@@ -5,7 +5,7 @@ import { Card, CardContent } from '../components/ui/card'
 import { Badge } from '../components/ui/badge'
 import { Trophy, Users, Target, Award, ArrowRight, Star, Clock, CheckCircle, Play, Shield, GraduationCap, Zap, Heart, TrendingUp } from 'lucide-react'
 import Layout from '../components/layout/Layout'
-import InstagramFeed from '../components/InstagramFeed'
+
 
 
 export default function LandingPage() {
