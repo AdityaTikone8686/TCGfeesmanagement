95f18cdb6ab3c26d41f971ea355c974389f4197a
AdityaTikone8686 <gigaminex@gmail.com>
2025-12-27 15:27:06 +0530

    Delete Backend/routes/socialRoutes.js

--- Backend/routes/socialRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { createPost, getPosts } from "../controllers/socialController.js";
-
-const router = express.Router();
-
-router.post("/", createPost); // Admin upload
-router.get("/", getPosts);    // Website display
-
-export default router;
